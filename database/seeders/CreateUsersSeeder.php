d56c58edc8786ff5327fd1e51d5e716612338ed5
butomyananthu <115061321+butomyananthu@users.noreply.github.com>
2024-01-14 12:52:09 +0530

    first commit

---
+++ database/seeders/CreateUsersSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CreateUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = [
+            [
+               'name'=>'Admin',
+               'email'=>'admin@gmail.com',
+                'user_type'=>'1',
+               'password'=> bcrypt('123456'),
+            ],
+            [
+               'name'=>'User',
+               'email'=>'user@gmail.com',
+                'user_type'=>'0',
+               'password'=> bcrypt('123456'),
+            ],
+        ];
+
+        foreach ($user as $key => $value) {
+            User::create($value);
+        }
+    }
+}
